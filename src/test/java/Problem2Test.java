0c9a48da4a198882432f95ac0f32025d7d2e0166
Leroy Smith <leroys2019@gmail.com>
2020-10-08 18:26:36 -0700

    TEst

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,7 +9,23 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+        int inputs[][] = {
+                {8, 6, 4, 2},
+                {1, 3, 2, 4},
+                {1, 2, 3, 4},
+        };
+
+        int expected[][] = {
+                {2, 4, 6, 8},
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+
+        };
+        for (int i = 0; i < inputs.length; i++){
+            Problem2.bubbleSort(inputs[i]);
+            assertArrayEquals(expected[i], inputs[i]);
+        }
     }
 }
+
+
